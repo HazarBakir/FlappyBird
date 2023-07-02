97e9c068e65f7af402459a85551e608963520f0f
Hazar <92859817+HazarBakir@users.noreply.github.com>
2023-07-02 10:34:56 +0300

    sound settings bugs fixed but not all.
    - Used PlayerPref for setting and saving the sound values.

--- FlappyBird/Assets/Scripts/MusicVolumeSetter.cs
+++ FlappyBird/Assets/Scripts/MusicVolumeSetter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicVolumeSetter : MonoBehaviour
 {
@@ -10,26 +11,46 @@ public class MusicVolumeSetter : MonoBehaviour
     private AudioSource jumpSoundEffect;
     [SerializeField]
     private AudioSource deathSoundEffect;
+    public Slider volumeSlider;
+    public Slider sfxSlider;
 
 
-    private float musicVolume = 1f;
-    private float sfxVolume = 1f;
-
-    void Update()
+    //private float musicVolume = 1f;
+    float sfxVolume;
+    float musicVolume;
+    private void Start()
     {
-        audioSource.volume = musicVolume;
-        jumpSoundEffect.volume = sfxVolume;
-        deathSoundEffect.volume = sfxVolume;
+        musicVolume = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        volumeSlider.value = musicVolume;
+        SetVolume(musicVolume);
+
+        sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 1f);
+        sfxSlider.value = sfxVolume;
+        SetSFX(sfxVolume);
     }
-    
-    public void SetVolume(float volume)
+    public void SaveVolume()
     {
-        musicVolume = volume;
+        // Slider'ın değerini alın ve PlayerPrefs'e kaydedin
+        float musicVolume = volumeSlider.value;
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+        PlayerPrefs.Save(); // Değişiklikleri kaydedin
+    }
+    public void SaveSFX()
+    {
+        float sfxVolume = volumeSlider.value;
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+        PlayerPrefs.Save(); // Değişiklikleri kaydedin
     }
     public void SetSFX(float volume)
     {
-        sfxVolume = volume;
+        jumpSoundEffect.volume = volume;
+        deathSoundEffect.volume = volume;
     }
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
     public void NoMusic()
     {
         musicVolume = 0;
